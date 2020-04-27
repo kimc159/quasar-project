3472d9d78d5db585963c732a78358fc39315e18d
juyeol <ljykimc159@gmail.com>
2020-04-27 19:43:04 +0900

    Menu page 추가

---
+++ src/pages/MenuPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    My Menu Page!
+
+    <q-btn
+      @click="goSearch"
+    >
+    검색
+    </q-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+  methods: {
+    goSearch () {
+      this.$router.push({ path: '/search' })
+    }
+  }
+}
+</script>
