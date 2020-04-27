b35af7278072cf57297b5de09634288140711b68
juyeol <ljykimc159@gmail.com>
2020-04-27 19:42:43 +0900

    search page 추가

---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- content -->
+    search Page
+
+    <search-box></search-box>
+    <result-list></result-list>
+  </div>
+</template>
+
+<script>
+import SearchBox from '../components/SearchBox'
+import ResultList from '../components/ResultList'
+
+export default {
+  components: {
+    SearchBox,
+    ResultList
+  }
+  // name: 'PageName',
+}
+</script>
